fef34e3033322504c5e9f3254856c4df34fe3b2d
Stergianni <stella.orfanidis@hotmail.com>
2025-02-24 20:30:58 +0100

    Setting up the environment

---
+++ travelRecommendation/travel_recommendation.js
@@ -0,0 +1,23 @@
+[
+    {
+      "city": "Paris",
+      "country": "France",
+      "attractions": [
+        "Eiffel Tower",
+        "Louvre Museum",
+        "Notre-Dame Cathedral"
+      ],
+      "best_time_to_visit": "April to June, September to November"
+    },
+    {
+      "city": "New York",
+      "country": "USA",
+      "attractions": [
+        "Statue of Liberty",
+        "Central Park",
+        "Times Square"
+      ],
+      "best_time_to_visit": "April to June, September to November"
+    }
+  ]
+  
